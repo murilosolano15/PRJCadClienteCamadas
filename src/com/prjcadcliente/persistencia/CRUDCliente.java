88cba927a7a050ea09e2ba409428f8d18ad6c1df
= <=>
2019-05-16 14:27:51 -0300

    criação do metodo pesquisar Todos

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -222,7 +222,52 @@ String msg = "";
 	}
 	
 	public List<Cliente> PesquisarTodos() {
-		return null;
+		
+		List<Cliente> lista = new ArrayList<Cliente>(); //array é um do lado do outro com"," lista é um embaixo do outro
+		
+		try {
+			//carregar o drive de comunicação com o banco de dados 
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+			
+			//Chamar o gerenciado de driver
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clientedb","root","");
+			
+			//Vamos criar a consulta para selecionar os cliente por nome 
+			String consulta = "select * from tbcliente";
+			
+			pst = con.prepareStatement(consulta);			
+			
+
+			//Vamos executar a consulta e guardar o resultado na variável rs 
+			rs = pst.executeQuery();
+			
+			/*
+			 * vamos pegar um cliente por vez que esta no rs e adicioná-lo a lista de clientes para, então retorná-la
+			 */
+			while(rs.next()) {
+				lista.add(new Cliente(
+						rs.getInt(0),
+						rs.getString(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getInt(4)
+						));
+				
+			}//Fim do while
+			
+		}//fim do try
+		
+		catch(SQLException ex) {
+		ex.printStackTrace();	
+		}
+		catch(Exception e) {
+			e.printStackTrace();	
+		}
+		finally {
+			try {con.close();} catch(Exception e) {e.printStackTrace();}
+		}
+		
+		return lista;
 	}
 	
 	
